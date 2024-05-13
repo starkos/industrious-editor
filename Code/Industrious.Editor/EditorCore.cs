4de2936710b26b53b1dc8dd313138bd60890c566
Jess Perkins <starkos@industriousone.com>
2024-05-13 18:47:47 -0400

    Load web code from embedded resource

--- Code/Industrious.Editor/EditorCore.cs
+++ Code/Industrious.Editor/EditorCore.cs
@@ -1,10 +1,19 @@
+using System.Reflection;
+
 namespace Industrious.Editor;
 
+/// <summary>
+///  Common editor logic, shared between all platform adapters.
+/// </summary>
 public class EditorCore
 {
 	public EditorCore (IEditorHost host)
 	{
-		const String content = "<html><head></head><body><p>Editor is installed.</p></body></html>";
-		host.LoadHtmlString (content);
+		var assembly = Assembly.GetExecutingAssembly ();
+		using var stream = assembly.GetManifestResourceStream ("Industrious.Editor.Resources.Editor.html");
+		using var streamReader = new StreamReader (stream!);
+
+		var html = streamReader.ReadToEnd ();
+		host.LoadHtmlString (html);
 	}
 }
