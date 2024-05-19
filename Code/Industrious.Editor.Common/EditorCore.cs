77d448f5f9e24ff48a76a6b3f09cee6f42d9d9ce
Jess Perkins <starkos@industriousone.com>
2024-05-19 12:04:39 -0400

    Remove placeholder content from editor

--- Code/Industrious.Editor.Common/EditorCore.cs
+++ Code/Industrious.Editor.Common/EditorCore.cs
@@ -7,7 +7,7 @@ public class EditorCore : IEditor
 	public EditorCore (IEditorWebView adapter)
 	{
 		var assembly = Assembly.GetExecutingAssembly ();
-		using var stream = assembly.GetManifestResourceStream ("Industrious.Editor.Common.Lexical.dist.index.html");
+		using var stream = assembly.GetManifestResourceStream ("Industrious.Editor.Lexical.dist.index.html");
 		using var streamReader = new StreamReader (stream!);
 
 		var html = streamReader.ReadToEnd ();
--- Code/Industrious.Editor.Common/Lexical/src/main.ts
+++ Code/Industrious.Editor.Common/Lexical/src/main.ts
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
 import './styles.css';
 
 import {registerDragonSupport} from '@lexical/dragon';
@@ -13,47 +6,37 @@ import {HeadingNode, QuoteNode, registerRichText} from '@lexical/rich-text';
 import {mergeRegister} from '@lexical/utils';
 import {createEditor} from 'lexical';
 
-import prepopulatedRichText from './prepopulatedRichText';
+// This can be moved out to index.html?
+// Do I need the wrapper?
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>Lexical Basic - Vanilla JS</h1>
-    <div class="editor-wrapper">
-      <div id="lexical-editor" contenteditable></div>
-    </div>
-    <h4>Editor state:</h4>
-    <textarea id="lexical-state"></textarea>
-  </div>
-`;
+<div class="editor-wrapper">
+  <div id="lexical-editor" contenteditable></div>
+</div>`;
+
 const editorRef = document.getElementById('lexical-editor');
-const stateRef = document.getElementById(
-  'lexical-state',
-) as HTMLTextAreaElement;
 
 const initialConfig = {
-  namespace: 'Vanilla JS Demo',
-  // Register nodes specific for @lexical/rich-text
+  namespace: 'Industrious.Editor',
   nodes: [HeadingNode, QuoteNode],
   onError: (error: Error) => {
     throw error;
   },
   theme: {
-    // Adding styling to Quote node, see styles.css
-    quote: 'PlaygroundEditorTheme__quote',
   },
 };
+
 const editor = createEditor(initialConfig);
 editor.setRootElement(editorRef);
 
-// Registring Plugins
 mergeRegister(
   registerRichText(editor),
   registerDragonSupport(editor),
   registerHistory(editor, createEmptyHistoryState(), 300),
 );
 
-editor.update(prepopulatedRichText, {tag: 'history-merge'});
+// Listen for changes - will eventually want to surface this to host
 
-editor.registerUpdateListener(({editorState}) => {
-  stateRef!.value = JSON.stringify(editorState.toJSON(), undefined, 2);
-});
+//editor.registerUpdateListener(({editorState}) => {
+//  stateRef!.value = JSON.stringify(editorState.toJSON(), undefined, 2);
+//});
